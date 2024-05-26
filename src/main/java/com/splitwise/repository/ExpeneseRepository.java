57d18d97b3b69a9fe68f954edf3ad14b2f30a00f
sanjayhare <sanjayghare9595@gmail.com>
2024-05-26 11:07:01 +0530

    when we will delete member from group if expense are pending for that member we cannot delete member
    if expenses are not present we will delete member from group and expenses

--- src/main/java/com/splitwise/repository/ExpeneseRepository.java
+++ src/main/java/com/splitwise/repository/ExpeneseRepository.java
@@ -4,5 +4,10 @@ import com.splitwise.entity.Expenses;
 import com.splitwise.entity.Group;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ExpeneseRepository  extends MongoRepository<Expenses, Long> {
+
+    List<Expenses> findByGroupId(Long id);
 }
--- src/main/java/com/splitwise/service/GroupService.java
+++ src/main/java/com/splitwise/service/GroupService.java
@@ -1,9 +1,13 @@
 package com.splitwise.service;
 
+import com.splitwise.entity.Contributers;
+import com.splitwise.entity.Expenses;
 import com.splitwise.entity.Group;
 import com.splitwise.entity.Members;
 import com.splitwise.exception.RecordAlreadyExistsException;
 import com.splitwise.exception.ResourceNotFoundException;
+import com.splitwise.exception.SplitWiseMessegeException;
+import com.splitwise.repository.ExpeneseRepository;
 import com.splitwise.repository.GroupRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +20,9 @@ public class GroupService {
 
     @Autowired
     private GroupRepository groupRepository;
+
+    @Autowired
+    private ExpeneseRepository expeneseRepository;
     @Autowired
     private SequenceGeneratorService sequenceGeneratorService;
 
@@ -37,17 +44,20 @@ public class GroupService {
         group.setTitle(group.getTitle().trim());
         groupRepository.save(group);
     }
+
     public List<Group> getGroupList() {
         List<Group> groups = groupRepository.findAll();
         return groups;
     }
+
     public Optional<Group> getGroup(Long id) {
         Optional<Group> group = groupRepository.findById(id);
-        if(group.isEmpty()){
+        if (group.isEmpty()) {
             throw new ResourceNotFoundException("Group", "GroupID", String.valueOf(id));
         }
         return group;
     }
+
     public boolean deleteGroup(Long id) {
         boolean isDeleted = false;
         groupRepository.deleteById(id);
@@ -55,16 +65,40 @@ public class GroupService {
         return isDeleted;
     }
 
-    public boolean deleteMember(Long gId , Long mId ) {
+    public boolean deleteMember(Long gId, Long mId) {
         boolean isDeleted = false;
         Optional<Group> group = groupRepository.findById(gId);
-        if(group.isEmpty()){
+        if (group.isEmpty()) {
             throw new ResourceNotFoundException("Group", "GroupID", String.valueOf(gId));
         }
+        List<Expenses> expenses = expeneseRepository.findByGroupId(gId);
+        Expenses expenses1 = new Expenses();
+        for (int i = 0; i < expenses.size(); i++) {
+            expenses1 = expenses.get(i);
+            List<Contributers> contributers = expenses1.getContributers();
+
+            Contributers contributers1 = new Contributers();
+            for (int j = 0; j < contributers.size(); j++) {
+
+                contributers1 = contributers.get(j);
+                System.out.println("Invalid Split type contributers1" + contributers1.getContId() + ":" + contributers1.getAmount());
+                if (contributers1.getContId() == mId) {
+
+                    if (Double.parseDouble(String.valueOf(contributers1.getAmount())) > 0.00) {
+                        throw new SplitWiseMessegeException("Expenses are pending for this Contributer");
+                    }
+                    contributers.remove(contributers1);
+                }
+            }
+            expenses1.setContributers(contributers);
+            expeneseRepository.save(expenses1);
+        }
         List<Members> members = group.get().getMembers();
-        for(Members members1 : members)
-        {
-            if(members1.getMemberId()== mId){
+        Members members1 = new Members();
+
+        for (int i = 0; i < members.size(); i++) {
+            members1 = members.get(i);
+            if (members1.getMemberId() == mId) {
                 members.remove(members1);
             }
         }
@@ -77,7 +111,7 @@ public class GroupService {
     public boolean updateGroup(Group group) {
         boolean isUpdated = false;
         Optional<Group> group1 = groupRepository.findById(group.getGroupId());
-        if(group1.isEmpty()){
+        if (group1.isEmpty()) {
             throw new ResourceNotFoundException("Group", "GroupID", String.valueOf(group.getGroupId()));
         }
         List<Members> members = group.getMembers();
